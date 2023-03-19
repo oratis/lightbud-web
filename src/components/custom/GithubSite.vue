92daf4878d18ae1b05b47cbcd96c26d0767cc349
oratis <wangharp@gmail.com>
2023-03-20 01:39:13 +0800

    图片修改

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
     <span>访问帮助</span>
-    <a href="https://appkon.com" target="_blank" class="text-blue-500">
+    <a href="https://appkon.com/lightbud" target="_blank" class="text-blue-500">
       Appkon Innovation
     </a>
   </div>
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://github.com/oratis/lightbud-web/blob/main/src/assets/avatar.png',
+      name: '轻伴 App',
+      description: '提点意见 <a href="https://appkon.com/feedback" class="text-blue-500" target="_blank" >反馈</a>',
     },
   }
 }
