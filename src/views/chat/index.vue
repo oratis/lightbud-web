ad7910f58640d4b07878991836ec07c8496d67df
oratis <wangharp@gmail.com>
2023-03-20 19:59:23 +0800

    新增setting和info

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -475,8 +475,8 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
-            </div>
+              <span>已连接到你的AI伙伴</span>
+           </div>
           </template>
           <template v-else>
             <div>
