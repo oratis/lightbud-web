97eaa1e384d2cd92bacbde04f3427bb0eaf3f80f
oratis <wangharp@gmail.com>
2023-03-21 16:25:36 +0800

    Update index.vue

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -6,23 +6,10 @@ const router = useRouter()
 import { defineComponent } from 'vue'
 import { NInput, NButton } from 'naive-ui'
 
-const aiInfo = defineComponent({
-  name: 'SaveText',
-  components: { NInput, NButton },
-  data() {
-    return {
-      userInput: ''
-    }
-  },
-  methods: {
-    saveText() {
+function saveInfo():void {
       // 将用户输入的文本保存到 localStorage 中
       localStorage.setItem('userText', this.userInput)
     }
-  }
-})
-
-export default aiInfo
 
 </script>
 
@@ -30,17 +17,20 @@ export default aiInfo
   <div class="flex flex-col w-full h-full">
     <div class="px-4 m-auto space-y-4 text-left max-[400px]">
       <header class="space-y-2">
-        <h2 class="text-2xl text-left text-slate-800 dark:text-neutral-200">
-            <n-input v-model="userInput" placeholder="请输入文本"></n-input>
-        </h2>
         <p class="text-base text-left text-slate-500 dark:text-slate-500">
           你的AIID是'uuid'
         </p>
+
+        <h2 class="text-2xl text-left text-slate-800 dark:text-neutral-200">
+            <p>你如何称呼你的AI伙伴：</p>
+            <n-input v-model="userInput" placeholder="取个名字吧"></n-input>
+        </h2>
+
         <div class="flex items-center justify-center text-center">
         </div>
       </header>
-      <NButton type="primary" @click="save">
-        保存设置
+      <NButton type="primary" @click="saveInfo">
+        {{ $t('common.save') }}
       </NButton>
     </div>
   </div>
