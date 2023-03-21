2e03cc093d825258a3bbbe346cc480d444e25272
oratis <wangharp@gmail.com>
2023-03-21 15:59:56 +0800

    Update index.vue

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -1,31 +1,45 @@
 <script lang="ts" setup>
-import { NButton } from 'naive-ui'
 import { useRouter } from 'vue-router'
-import Icon500 from '@/icons/500.vue'
 
 const router = useRouter()
 
-function goHome() {
-  router.push('/')
-}
+import { defineComponent } from 'vue'
+import { NInput, NButton } from 'naive-ui'
+
+export default defineComponent({
+  name: 'SaveText',
+  components: { NInput, NButton },
+  data() {
+    return {
+      userInput: ''
+    }
+  },
+  methods: {
+    saveText() {
+      // 将用户输入的文本保存到 localStorage 中
+      localStorage.setItem('userText', this.userInput)
+    }
+  }
+})
+
 </script>
 
 <template>
   <div class="flex flex-col w-full h-full">
     <div class="px-4 m-auto space-y-4 text-left max-[400px]">
       <header class="space-y-2">
-        <h2 class="text-2xl font-bold text-center text-slate-800 dark:text-neutral-200">
-          500
+        <h2 class="text-2xl text-left text-slate-800 dark:text-neutral-200">
+            <n-input v-model="userInput" placeholder="请输入文本"></n-input>
         </h2>
-        <p class="text-base text-center text-slate-500 dark:text-slate-500">
-          Server error
+        <p class="text-base text-left text-slate-500 dark:text-slate-500">
+          你的AIID是'uuid'
         </p>
         <div class="flex items-center justify-center text-center">
           <Icon500 class="w-[300px]" />
         </div>
       </header>
-      <NButton type="primary" @click="goHome">
-        Go to Home
+      <NButton type="primary" @click="save">
+        保存设置
       </NButton>
     </div>
   </div>
