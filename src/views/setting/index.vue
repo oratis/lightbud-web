ea8c75a07e0d17f60df08a7d9aba1bb33700f3ae
oratis <wangharp@gmail.com>
2023-03-22 20:57:51 +0800

    1

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -20,7 +20,7 @@ function goHome() {
           版本号:0.1
         </p>
         <div class="flex items-center justify-center text-center">
-          <img src="../../assets/version.png"/>
+          <img src="@/assets/version.png"/>
         </div>
       </header>
       <NButton type="primary" @click="goHome">
