1f75ba564ae01dda9cd76cb2b42764106e02230b
oratis <wangharp@gmail.com>
2023-03-22 19:12:04 +0800

    12

--- src/store/modules/prompt/helper.ts
+++ src/store/modules/prompt/helper.ts
@@ -3,8 +3,6 @@ import PromptInput from '../../../assets/prompts.json'
 
 const LOCAL_NAME = 'promptStore'
 
-
-
 export type PromptListItem = {
   act: string;
   prompt: string;
@@ -25,5 +23,12 @@ export function setLocalPromptList(promptStore: PromptStore): void {
   ss.set(LOCAL_NAME, promptStore)
 }
 
-// 使用JSON数据更新promptList
-setLocalPromptList({promptList: PromptInput.promptList})
+// 创建新窗口对象列表
+const promptList: PromptList = PromptInput.promptList.map(
+  ({ act, prompt }: { act: string; prompt: string }) => ({
+    act,
+    prompt
+  })
+)
+
+setLocalPromptList({ promptList });
