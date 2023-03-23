549c76d6059d8e42e435b5bd18cd46df7b3f3d29
oratis <wangharp@gmail.com>
2023-03-23 11:28:46 +0800

    Update helper.ts

--- src/store/modules/prompt/helper.ts
+++ src/store/modules/prompt/helper.ts
@@ -1,25 +1,33 @@
-import { ss } from '@/utils/storage'
-import promptInput from '@/assets/prompts.json'
 
-const LOCAL_NAME = 'promptStore'
+import { ss } from '@/utils/storage';
+import promptInput from '@/assets/prompts.json';
 
-export type PromptList = []
+const LOCAL_NAME = 'promptStore';
 
 export interface PromptItem {
-  key: string
-  value: string
+  key: string;
+  value: string;
 }
 
 export interface PromptStore {
-  promptList: PromptItem[]
+  promptList: PromptItem[];
 }
 
 export function getLocalPromptList(): PromptStore {
-  const promptStore: PromptStore | undefined = ss.get(LOCAL_NAME)
-  return promptStore ?? { promptList: promptInput }
+  const promptsFromStorage: PromptStore | undefined = ss.get(LOCAL_NAME);
+
+  if (promptsFromStorage !== undefined) {
+    return promptsFromStorage;
+  }
+
+  // 基于存储 json 直接提供全新提示列表
+  if (promptInput.promptList.length > 0){
+     return { promptList : [...promptInput.promptList] };
+  }
+
+  return { promptList: [] };
 }
 
 export function setLocalPromptList(promptStore: PromptStore): void {
-  ss.set(LOCAL_NAME, promptStore)
+  ss.set(LOCAL_NAME, promptStore);
 }
-
