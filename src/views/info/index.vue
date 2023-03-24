23f0c634babb32ea068c1e7ccac170c9283afb79
oratis <wangharp@gmail.com>
2023-03-24 16:35:28 +0800

    223

---
+++ src/views/info/index.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import PromptStore from '@/components/common'
+
+export default {
+    name: 'Info',
+    components: {
+      PromptStore,
+    },
+  }
+
+</script>
+
+<template>
+<div class="flex flex-col w-full h-full">
+  <div class="px-4 m-auto space-y-4 text-center max-[400px]">
+		   <PromptStore></PromptStore>
+  </div>
+</div>
+</template>
