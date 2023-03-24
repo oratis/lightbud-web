d3a325565fea8de0eb2a9d568116fad2f16d8eb4
oratis <wangharp@gmail.com>
2023-03-24 22:36:35 +0800

    Update index.vue

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -6,7 +6,6 @@ import List from './List.vue'
 import Footer from './Footer.vue'
 import { useAppStore, useChatStore } from '@/store'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
-import { PromptStore } from '@/components/common'
 
 const appStore = useAppStore()
 const chatStore = useChatStore()
@@ -77,11 +76,7 @@ watch(
         <div class="flex-1 min-h-0 pb-4 overflow-hidden">
           <List />
         </div>
-        <div class="p-4">
-          <NButton block @click="show = true">
-            Prompt Store
-          </NButton>
-        </div>
+
       </main>
       <Footer />
     </div>
