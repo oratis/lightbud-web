41dbed516d82b21024c02d00993e9448ee14e4b0
oratis <wangharp@gmail.com>
2023-03-24 18:29:25 +0800

    Update index.vue

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -353,7 +353,7 @@ export default {
   }
 
 
-<template>
+</template>
 <div class="flex flex-col w-full h-full">
   <div class="px-4 m-auto space-y-4 text-center max-[400px]">
 		   <PromptStore></PromptStore>
